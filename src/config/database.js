84a899f078a4addb05cd00f16659bb020d0a23f3
Leonel <leonelpelusi@gmail.com>
2019-12-12 16:55:01 -0300

    config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,25 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'concrete',
+  host: 'ec2-174-129-255-106.compute-1.amazonaws.com',
+  username: 'bkhrivecemstkk',
+  password: '6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f',
+  database: 'dfhtrduie4vf9',
   define: {
     timestamp: true,
     underscored: true,
     underscoredAll: true,
   },
 };
+
+// module.exports = {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   username: 'postgres',
+//   password: 'docker',
+//   database: 'concrete',
+//   define: {
+//     timestamp: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
