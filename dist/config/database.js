3028e94b2f55e47f0499a165e0a6db947402d77e
Leonel Moura Pelusi <leonelpelusi@gmail.com>
2019-12-24 10:09:23 -0300

    new build config/database.js

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,29 +1,29 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _pg = require('pg'); var _pg2 = _interopRequireDefault(_pg);
+"use strict";// import Client from 'pg';
 
-const client = new (0, _pg2.default)({
-  connectionString: process.env.DATABASE_URL2,
-	ssl: true,
-	timestamps: true,
-	underscored: true,
-	underscoredAll: true
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL2,
+// 	ssl: true,
+// 	timestamps: true,
+// 	underscored: true,
+// 	underscoredAll: true
+// });
 
-client.connect();
+// client.connect();
 
-// require("dotenv/config");
+require("dotenv/config");
 
-// module.exports = {
-// 	dialect: "postgres",
-// 	host: process.env.DB_HOST || "ec2-174-129-255-106.compute-1.amazonaws.com",
-// 	port: process.env.DB_PORT || 5432,
-// 	username: process.env.DB_USER || "bkhrivecemstkk",
-// 	password:
-// 		process.env.DB_PASS ||
-// 		"6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f",
-// 	database: process.env.DB_NAME || "dfhtrduie4vf9",
-// 	define: {
-// 		timestamps: true,
-// 		underscored: true,
-// 		underscoredAll: true
-// 	}
-// };
+module.exports = {
+	dialect: "postgres",
+	host: process.env.DB_HOST || "ec2-174-129-255-106.compute-1.amazonaws.com",
+	port: process.env.DB_PORT || 5432,
+	username: process.env.DB_USER || "bkhrivecemstkk",
+	password:
+		process.env.DB_PASS ||
+		"6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f",
+	database: process.env.DB_NAME || "dfhtrduie4vf9",
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true
+	}
+};
