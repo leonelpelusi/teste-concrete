86a7af19299d5b789852088aacab4fc31025b372
Leonel Moura Pelusi <leonelpelusi@gmail.com>
2019-12-24 10:00:17 -0300

    fix Sequelize package.json

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,29 @@
-require("dotenv/config");
+import Client from 'pg';
 
-module.exports = {
-	dialect: "postgres",
-	host: process.env.DB_HOST || "ec2-174-129-255-106.compute-1.amazonaws.com",
-	port: process.env.DB_PORT || 5432,
-	username: process.env.DB_USER || "bkhrivecemstkk",
-	password:
-		process.env.DB_PASS ||
-		"6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f",
-	database: process.env.DB_NAME || "dfhtrduie4vf9",
-	define: {
-		timestamps: true,
-		underscored: true,
-		underscoredAll: true
-	}
-};
+const client = new Client({
+  connectionString: process.env.DATABASE_URL2,
+	ssl: true,
+	timestamps: true,
+	underscored: true,
+	underscoredAll: true
+});
+
+client.connect();
+
+// require("dotenv/config");
+
+// module.exports = {
+// 	dialect: "postgres",
+// 	host: process.env.DB_HOST || "ec2-174-129-255-106.compute-1.amazonaws.com",
+// 	port: process.env.DB_PORT || 5432,
+// 	username: process.env.DB_USER || "bkhrivecemstkk",
+// 	password:
+// 		process.env.DB_PASS ||
+// 		"6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f",
+// 	database: process.env.DB_NAME || "dfhtrduie4vf9",
+// 	define: {
+// 		timestamps: true,
+// 		underscored: true,
+// 		underscoredAll: true
+// 	}
+// };
