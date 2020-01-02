ac84db45ce14266fcd20a56a06c3527922ebe23c
Leonel Moura Pelusi <leonelpelusi@gmail.com>
2020-01-02 14:29:42 -0300

    fixing database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,28 +1,15 @@
-// module.exports = {
-// 	dialect: "postgres",
-// 	host: process.env.DB_HOST || "ec2-174-129-255-106.compute-1.amazonaws.com",
-// 	port: process.env.DB_PORT || 5432,
-// 	username: process.env.DB_USER || "bkhrivecemstkk",
-// 	password:
-// 		process.env.DB_PASS ||
-// 		"6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f",
-// 	database: process.env.DB_NAME || "dfhtrduie4vf9",
-// 	define: {
-// 		timestamps: true,
-// 		underscored: true,
-// 		underscoredAll: true
-// 	}
-// };
-
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'postgres',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+	dialect: "postgres",
+	host: process.env.DB_HOST || "ec2-174-129-255-106.compute-1.amazonaws.com",
+	port: process.env.DB_PORT || 5432,
+	username: process.env.DB_USER || "bkhrivecemstkk",
+	password:
+		process.env.DB_PASS ||
+		"6c1a8f4ba64b8a160bd741895ff309b15945aa09591b906f85affde2e9fb5d9f",
+	database: process.env.DB_NAME || "dfhtrduie4vf9",
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true
+	}
 };
